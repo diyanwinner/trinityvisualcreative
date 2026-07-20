d8d469b4fc11d7e5c692a5a36d93856e480bb96a
diyanwinner <136164968+diyanwinner@users.noreply.github.com>
2026-07-20 16:42:27 +0700

    Update config.js

--- brief/config.js
+++ brief/config.js
@@ -1,4 +1,3 @@
 window.TRINITY_BRIEF_CONFIG = {
-  // Tempel URL deployment Google Apps Script yang berakhiran /exec.
   appScriptUrl: "https://script.google.com/macros/s/AKfycbzf-eWVTW3_5s9OHRo9CQwVIhufSd-Z2ejbsRfhR8HH0rvfV6drvLLZbvYHWexYiZKKMA/exec"
 };
