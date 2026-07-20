f58ca4f04a68d8f6921f2f6031a847de18aaa475
diyanwinner <136164968+diyanwinner@users.noreply.github.com>
2026-07-20 16:31:37 +0700

    Add files via upload

---
+++ brief/config.js
@@ -0,0 +1,4 @@
+window.TRINITY_BRIEF_CONFIG = {
+  // Tempel URL deployment Google Apps Script yang berakhiran /exec.
+  appScriptUrl: "https://script.google.com/macros/s/AKfycbzf-eWVTW3_5s9OHRo9CQwVIhufSd-Z2ejbsRfhR8HH0rvfV6drvLLZbvYHWexYiZKKMA/exec"
+};
